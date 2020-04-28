5ac6c59c46111ffcddc208ff20ba51b953d40c9d
aditya0072001 <tripathi.aditya1660@gmail.com>
2020-04-28 19:35:50 +0530

    configuring files

--- apply/views.py
+++ apply/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def homepage(request):
-    return render(request=request,template_name="posinegavi/home.html")
+    return render(request=request,template_name="apply/home.html")
