ae77ed3f52334d8856ce83a7ae65d69364f47a55
Tripathi Aditya Prakash <41997646+aditya0072001@users.noreply.github.com>
2021-05-24 12:07:49 +0530

    adding heroku

--- apply/settings.py
+++ apply/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -28,6 +29,8 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 WSGI_APPLICATION = 'apply.wsgi.application'
+
+
 
 # Application definition
 
@@ -122,3 +125,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+django_heroku.settings(locals())
